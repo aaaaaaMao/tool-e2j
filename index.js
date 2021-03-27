9c3b6164cdd20b5fe34ce50bfe626f8f7bd8df5a
maojie <j.mao@maimemo.com>
2021-03-27 23:52:31 +0800

    feat(index): add some options

--- index.js
+++ index.js
@@ -2,27 +2,51 @@
 
 const fs = require('fs')
 const XLSX = require('xlsx')
+const {Command} = require('commander')
+
+const program = new Command()
+program
+    .option('-o, --out <file>', 'output file')
+    .option('--split', 'split by sheet name')
 
 function run(argv) {
-    if (!argv || argv.length < 2) {
-        console.log("basic usage: tool-e2j <.xlsx> <.json>")
+    
+    program.parse()
+    const options = program.opts()
+
+    if (!argv || argv.length < 1) {
+        console.log("basic usage: tool-e2j <.xlsx>")
     } else {
+        const inFile = argv[0]
+        if (!inFile.endsWith('.xlsx')) {
+            throw new Error('Error input file: ' + inFile)
+        }
+        console.log(options)
+        const outFile = options.out || inFile.replace('.xlsx', '.json')
 
-        const workbook = XLSX.readFile(argv[0])
-
+        const workbook = XLSX.readFile(inFile)
         const result = []
         for (const sheetName of workbook.SheetNames) {
             /* Get worksheet */
             const worksheet = workbook.Sheets[sheetName]
             const data = XLSX.utils.sheet_to_json(worksheet)
-            result.push({
-                sheet_name: sheetName,
-                data,
-            })
+
             console.log(data.length, sheetName)
+            if (options.split) {
+                const file = outFile.replace('.json', '-') + sheetName + '.json'
+                fs.writeFileSync(file, JSON.stringify(data, null, 2), 'utf8')
+                console.log('save ', sheetName)
+            } else {
+                result.push({
+                    sheet_name: sheetName,
+                    data,
+                })
+            }
+
         }
-        fs.writeFileSync(argv[1], JSON.stringify(result, null, 2), 'utf8')
-    
+        if (!options.split) {
+            fs.writeFileSync(outFile, JSON.stringify(result, null, 2), 'utf8')
+        }
         console.log('Done :)')
     }
 }
